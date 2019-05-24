8699013e528d4f48d48fdcabacc4ff0df691c5ac
Pau Barbarroja Claramunt <pau29jastermes@hotmail.com>
2019-05-24 20:49:23 +0200

    haciendo pruebas hr.employee

--- addons/primer_modulo/atletas/atleta.py
+++ addons/primer_modulo/atletas/atleta.py
@@ -2,15 +2,7 @@
 from odoo import models, fields, api
 
 
-class jas_atleta(models.Model):
-    _name = 'jas.atleta'
-    _rec_name = 'nombre'
+class berp_atleta(models.Model):
+    _inherit = "hr.employee"
 
     nombre              = fields.Char(string='Nombre',required=True)
-    apellidos           = fields.Char(string='Apellido',required=True)
-    ficha               = fields.Date(string='ficha',required=True)
-    dni                 = fields.Char(string='dni',required=True)
-    edad                = fields.Date(string='edad',required=True)
-    genero              = fields.Selection([('1', 'Masculino'),('2', 'Femenino')],string='genero',required=True)
-    telefono            = fields.Char(string='telefono',required=True)
-    mail                = fields.Char(string='mail',required=True)
