9e1a83eb7bf0fb2ca6dc47e9e77e0bd6d73c05e6
Pau Barbarroja Claramunt <pau29jastermes@hotmail.com>
2019-05-25 16:40:53 +0200

    funcional

--- addons/b-erp/atletas/atleta.py
+++ addons/b-erp/atletas/atleta.py
@@ -7,3 +7,14 @@ class berp_atleta(models.Model):
     _inherit = "hr.employee"
 
     nombre              = fields.Char(string='Nombre',required=True)
+    genero = fields.Selection([('1', 'Masculino'),('2', 'Femenino')],string='Genero',required=True)
+
+    '''
+    apellidos           = fields.Char(string='Apellido',required=True)
+    ficha               = fields.Date(string='ficha',required=True)
+    dni                 = fields.Char(string='dni',required=True)
+    edad                = fields.Date(string='edad',required=True)
+    genero              = fields.Selection([('1', 'Masculino'),('2', 'Femenino')],string='genero',required=True)
+    telefono            = fields.Char(string='telefono',required=True)
+    mail                = fields.Char(string='mail',required=True)
+    '''
