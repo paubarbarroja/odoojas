613a3c47174c045dd91970547ee1b64f018f91e5
Pau Barbarroja Claramunt <pau29jastermes@hotmail.com>
2019-05-26 12:53:37 +0200

    nueva vista como la de empleados pero para atletas.

--- addons/b-erp/atletas/atleta.py
+++ addons/b-erp/atletas/atleta.py
@@ -3,6 +3,17 @@ from odoo import models, fields, api
 
 
 class berp_atleta(models.Model):
+    _name = "berp.atleta"
     _inherit = "hr.employee"
 
-    nombre = fields.Char(string="nombre")
+    genero = fields.Selection([('1', 'Masculino'),('2', 'Femenino')],string='Genero')
+
+    '''
+    apellidos           = fields.Char(string='Apellido',required=True)
+    ficha               = fields.Date(string='ficha',required=True)
+    dni                 = fields.Char(string='dni',required=True)
+    edad                = fields.Date(string='edad',required=True)
+    genero              = fields.Selection([('1', 'Masculino'),('2', 'Femenino')],string='genero',required=True)
+    telefono            = fields.Char(string='telefono',required=True)
+    mail                = fields.Char(string='mail',required=True)
+    '''
