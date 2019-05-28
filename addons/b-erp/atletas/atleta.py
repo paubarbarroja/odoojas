51969aa3ef984da87700d0073024216168880691
Pau Barbarroja Claramunt <pau29jastermes@hotmail.com>
2019-05-28 21:15:24 +0200

    .

--- addons/b-erp/atletas/atleta.py
+++ addons/b-erp/atletas/atleta.py
@@ -10,6 +10,8 @@ class berp_atleta(models.Model):
     @api.model
     def create(self, values):
         record = super(berp_atleta, self).create(values)
+        _logger.debug(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ")
         _logger.debug(" ---------------- >>   Hello %s",record)
-
+        _logger.debug(" ---------------- >>   Hello %s",values)
+        _logger.debug(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ")
         return record
