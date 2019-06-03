d3683fa313ae22fa276edb903cefdee146fc9f43
paubarbarroja <pau29jastermes@hotmail.com>
2019-06-03 23:00:37 +0200

    .

--- addons/berp/atletas/atleta.py
+++ addons/berp/atletas/atleta.py
@@ -5,23 +5,20 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class berp_atleta(models.Model):
-    _inherit = "hr.employee"
+    _name = "berp.atleta"
 
-
-
-    @api.model
-    def create(self, values):
-        '''vals = {
-            'name': values['name'],
-            'login': values['work_email'],
-            'email': values['work_email'],
-        }
-        user = self.env['res.users'].create(vals)
-        values.update({'user_id': user.id})'''
-        record = super(berp_atleta, self).create(values)
-        return record
-
-    @api.multi
-    def unlink(self):
-        res = super(berp_atleta, self).unlink()
-        return res
+    nombre = fields.Char(string="Nombre")
+    tipo = fields.Many2many('berp.categoria',string="Tipo",placeholder="Atleta,Entrenador...")
+    correo = fields.Char(string="Correo-e")
+    telefono = fields.Char(string="Teléfono")
+    especialidad = fields.Selection([('1', 'Velocidad'),('2', 'Fondo'),('3', 'Medio Fondo'),('4', 'Lanzamientos'),('5', 'Saltos'),('6', 'Marcha Atlética')],string='Especialidad')
+    nacionalidad = fields.Many2one('res.country',string="Nacionalidad")
+    dni = fields.Char(string="DNI")
+    ficha = fields.Char(string="Ficha")
+    genero = fields.Selection([('1', 'Masculino'),('2', 'Femenino')],string='Genero')
+    fecha_nac = fields.Date(string="Fecha de Nacimiento")
+    direccion = fields.Char(string="Dirección")
+    cuenta_banc = fields.Char(string="Cuenta Bancaria")
+    nombre_cont = fields.Char(string="Contacto de Emergencia")
+    telef_cont = fields.Char(string="Teléfono de Emergencia")
+    imagen = fields.Binary('Imagen')
