92ffcc29cc95b8dd92ea719915bf23ad0a9998f6
paubarbarroja <pau29jastermes@hotmail.com>
2019-06-12 01:28:24 +0200

    Fenomenal,,, introduciento mejoras poco a poco

--- addons/berp/grupos_entreno/grupo_entreno.py
+++ addons/berp/grupos_entreno/grupo_entreno.py
@@ -11,3 +11,6 @@ class berp_grupo_entreno(models.Model):
 
     nombre = fields.Char(string="Nombre")
     atletas = fields.One2many('berp.socio','grupo_entreno',string="Atletas")
+
+
+# todo Poner en la vista form, en el tree de los atletas poner que tipo de socio es,,,,, atleta, entrenador, para saber quien es el entrenador i los atletas.....
