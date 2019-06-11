b2dd1cf09bca2f484f8b8d6c1354e803ea6dd0a5
paubarbarroja <pau29jastermes@hotmail.com>
2019-06-11 16:04:07 +0200

    funcionamiento grupos, menus, marcas, etc

--- addons/berp/marcas/marca.py
+++ addons/berp/marcas/marca.py
@@ -9,8 +9,12 @@ _logger = logging.getLogger(__name__)
 class berp_marca(models.Model):
     _name = "berp.marca"
 
-    #raise exceptions.except_orm(_('Error!!'), _('No has introducido bien la marca'))
+    # raise exceptions.except_orm(_('Error!!'), _('No has introducido bien la marca'))
 
+
+    '''######################################################################'''
+    '''########  Funcion para saber la categoria del Socio / Atleta  ########'''
+    '''######################################################################'''
     @api.multi
     def _get_categoria(self):
         if self.atleta:
@@ -41,11 +45,14 @@ class berp_marca(models.Model):
                                 else:
                                     if edad > 13:
                                         categoria = "Sub 16"
-
         else:
             categoria = ""
         return categoria
 
+
+    '''####################################################'''
+    '''########  Funcion onchange del campo atleta ########'''
+    '''####################################################'''
     @api.onchange('atleta')
     def onchange_socio(self):
         categoria = self._get_categoria()
@@ -54,7 +61,9 @@ class berp_marca(models.Model):
         self.marca = False
         self.marca_s = False
 
-
+    '''####################################################'''
+    '''########  Funcion onchange del campo prueba ########'''
+    '''####################################################'''
     @api.onchange('prueba')
     def onchange_prueba(self):
         if self.prueba:
@@ -64,22 +73,25 @@ class berp_marca(models.Model):
                 else:
                     self.hide = False
 
-
+    '''#####################################################'''
+    '''########  Funcion onchange del campo marca_s ########'''
+    '''#####################################################'''
     @api.onchange('marca_s')
     def onchange_marca_s(self):
-        if self.marca_s:
-            if self.marca_s.find("."):
-                tiempo = self.marca_s.split(".")
-                min_seg = tiempo[0] if len(tiempo) > 0 else '0'
-                centesimas = tiempo[1] if len(tiempo) > 1 else '0'
-                if min_seg.find(":") == 2:
-                    tiempo_v2 = min_seg.split(":")
-                    minutos = tiempo_v2[0] if len(tiempo_v2) > 0 else '0'
-                    segundos = tiempo_v2[1] if len(tiempo_v2) > 1 else '0'
-                else:
-                    segundos = min_seg
-                    minutos = '0'
-            self.marca = float(segundos)+(float(minutos)*60)+(float(centesimas)/100)
+        if not self.marca:
+            if self.marca_s:
+                if self.marca_s.find("."):
+                    tiempo = self.marca_s.split(".")
+                    min_seg = tiempo[0] if len(tiempo) > 0 else '0'
+                    centesimas = tiempo[1] if len(tiempo) > 1 else '0'
+                    if min_seg.find(":") == 2:
+                        tiempo_v2 = min_seg.split(":")
+                        minutos = tiempo_v2[0] if len(tiempo_v2) > 0 else '0'
+                        segundos = tiempo_v2[1] if len(tiempo_v2) > 1 else '0'
+                    else:
+                        segundos = min_seg
+                        minutos = '0'
+                self.marca = float(segundos)+(float(minutos)*60)+(float(centesimas)/100)
 
 
     def _get_puntos_hungaros(self):
@@ -95,20 +107,27 @@ class berp_marca(models.Model):
                             id = object.id.id
                 else:
                     if self.prueba.especialidad == '4' or self.prueba.especialidad == '5':
-                        while not puntos_por_prueba_ids:
-                            marca = round((marca - 0.01),2)
-                            puntos_por_prueba_ids = self.env['berp.puntos_hungaros'].search([('prueba', '=', self.prueba.id), ('marca', '=', marca)])
+                        if self.env['berp.puntos_hungaros'].search([('prueba', '=', self.prueba.id), ('marca', '<', marca), ('marca', '!=', 0.00)]):
+                            while not puntos_por_prueba_ids:
+                                marca = round((marca - 0.01),2)
+                                puntos_por_prueba_ids = self.env['berp.puntos_hungaros'].search([('prueba', '=', self.prueba.id), ('marca', '=', marca)])
                     else:
-                        while not puntos_por_prueba_ids:
-                            marca = round((marca - 0.01),2)
-                            puntos_por_prueba_ids = self.env['berp.puntos_hungaros'].search([('prueba', '=', self.prueba.id), ('marca', '=', marca)])
+                        if self.env['berp.puntos_hungaros'].search([('prueba', '=', self.prueba.id), ('marca', '>', marca), ('marca', '!=', 0.00)]):
+                            while not puntos_por_prueba_ids:
+                                marca = round((marca - 0.01),2)
+                                puntos_por_prueba_ids = self.env['berp.puntos_hungaros'].search([('prueba', '=', self.prueba.id), ('marca', '=', marca)])
                     for id in puntos_por_prueba_ids:
                         object = self.env['berp.puntos_hungaros'].browse(id)
                         if marca == object.id.marca:
                             id = object.id.id
+
+        if not id:
+            id = 1
         return id
 
-
+    '''###################################################'''
+    '''########  Funcion onchange del campo marca ########'''
+    '''###################################################'''
     @api.onchange('marca')
     def onchange_marca(self):
         if self.marca:
@@ -116,6 +135,8 @@ class berp_marca(models.Model):
                 self.marca_s = str(self.marca)
             self.puntos_hungaros = self._get_puntos_hungaros()
 
+
+    '''########  Override de la funcion Write ########'''
     @api.multi
     def write(self, values):
         categoria = self._get_categoria()
@@ -125,7 +146,9 @@ class berp_marca(models.Model):
         return res
 
 
-
+    '''################################################'''
+    '''########  Override de la funcion Create ########'''
+    '''################################################'''
     @api.model
     def create(self, values):
         id = False
@@ -161,28 +184,29 @@ class berp_marca(models.Model):
 
         if 'prueba' in values:
             prueba = self.env['berp.prueba'].browse(values['prueba'])
-            if 'marca_s' in values:
-                marca_s = values['marca_s']
-                if marca_s != "0.0":
-                    if marca_s.find("."):
-                        tiempo = marca_s.split(".")
-                        min_seg = tiempo[0] if len(tiempo) > 0 else '0'
-                        centesimas = tiempo[1] if len(tiempo) > 1 else '0'
-                        if min_seg.find(":") == 2:
-                            tiempo_v2 = min_seg.split(":")
-                            minutos = tiempo_v2[0] if len(tiempo_v2) > 0 else '0'
-                            segundos = tiempo_v2[1] if len(tiempo_v2) > 1 else '0'
-                        else:
-                            segundos = min_seg
-                            minutos = '0'
-                    values.update({'marca': (float(segundos) + (float(minutos) * 60) + (float(centesimas) / 100))})
+            if prueba.especialidad != '4' and prueba.especialidad != '5':
+                if 'marca_s' in values:
+                    marca_s = values['marca_s']
+                    if marca_s != "0.0":
+                        if marca_s.find("."):
+                            tiempo = marca_s.split(".")
+                            min_seg = tiempo[0] if len(tiempo) > 0 else '0'
+                            centesimas = tiempo[1] if len(tiempo) > 1 else '0'
+                            if min_seg.find(":") == 2:
+                                tiempo_v2 = min_seg.split(":")
+                                minutos = tiempo_v2[0] if len(tiempo_v2) > 0 else '0'
+                                segundos = tiempo_v2[1] if len(tiempo_v2) > 1 else '0'
+                            else:
+                                segundos = min_seg
+                                minutos = '0'
+                        values.update({'marca': (float(segundos) + (float(minutos) * 60) + (float(centesimas) / 100))})
             if 'marca' in values:
                 if values['marca'] != 0:
                     marca = values['marca']
                     puntos_por_prueba_ids = self.env['berp.puntos_hungaros'].search([('prueba', '=', prueba.id), ('marca', '=', marca)])
                     if puntos_por_prueba_ids:
-                        for id in puntos_por_prueba_ids:
-                            object = self.env['berp.puntos_hungaros'].browse(id)
+                        for ids in puntos_por_prueba_ids:
+                            object = self.env['berp.puntos_hungaros'].browse(ids)
                             if marca == object.id.marca:
                                 id = object.id.id
                                 if prueba.especialidad == '4' or prueba.especialidad == '5':
@@ -190,24 +214,34 @@ class berp_marca(models.Model):
                     else:
                         if prueba.especialidad == '4' or prueba.especialidad == '5':
                             if self.env['berp.puntos_hungaros'].search([('prueba', '=', prueba.id), ('marca', '<', marca), ('marca', '!=', 0.00)]):
-                                raise exceptions.except_orm(_('Error!!'), _(self.env['berp.puntos_hungaros'].search([('prueba', '=', prueba.id), ('marca', '<', marca), ('marca', '!=', 0.00)])))
-                                '''while not puntos_por_prueba_ids:
+                                while not puntos_por_prueba_ids:
                                     marca = round((marca - 0.01), 2)
                                     puntos_por_prueba_ids = self.env['berp.puntos_hungaros'].search([('prueba', '=', prueba.id), ('marca', '=', marca)])
-                                values.update({'marca_s': str(marca), 'hide': True})'''
+                                values.update({'marca_s': str(marca), 'hide': True})
+                            else:
+                                id = 1
                         else:
                             if self.env['berp.puntos_hungaros'].search([('prueba', '=', prueba.id), ('marca', '>', marca), ('marca', '!=', 0.00)]):
                                 while not puntos_por_prueba_ids:
                                     marca = round((marca - 0.01), 2)
                                     puntos_por_prueba_ids = self.env['berp.puntos_hungaros'].search([('prueba', '=', prueba.id), ('marca', '=', marca)])
-                        for id in puntos_por_prueba_ids:
-                                object = self.env['berp.puntos_hungaros'].browse(id)
+                            else:
+                                id = 1
+                        for ids in puntos_por_prueba_ids:
+                                object = self.env['berp.puntos_hungaros'].browse(ids)
                                 if marca == object.id.marca:
                                     id = object.id.id
 
         values.update({'categoria': categoria, 'puntos_hungaros': id})
         res = super(berp_marca, self).create(values)
         return res
+
+
+    '''###########################################'''
+    '''###########################################'''
+    '''########  Campos de la clase marca ########'''
+    '''###########################################'''
+    '''###########################################'''
 
     atleta = fields.Many2one('berp.socio',string="Socio")
     genero_atleta = fields.Selection(related="atleta.genero", store="True")
--- addons/berp/pistas/pista.py
+++ addons/berp/pistas/pista.py
@@ -6,9 +6,15 @@ _logger = logging.getLogger(__name__)
 
 class berp_pista(models.Model):
     _name = "berp.pista"
-    _rec_name = "nombre"
+    _rec_name = "nombre_mostrado"
+
+    @api.multi
+    def _get_nombre_mostrado(self):
+        for record in self:
+            record.nombre_mostrado = record.nombre+"("+record.ciudad+")"
 
 
-    nombre = fields.Char(string="Nombre")
+    nombre = fields.Char(string="Nombre Pista")
     ciudad = fields.Char(string="Ciudad")
     direccion = fields.Char(string="Dirección")
+    nombre_mostrado = fields.Char(compute="_get_nombre_mostrado",string="Nombre")
