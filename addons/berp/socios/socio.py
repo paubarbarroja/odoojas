bb24bc075cf76d82557e25ecf52eb1dd1ec091c7
paubarbarroja <pau29jastermes@hotmail.com>
2019-08-19 16:41:01 +0200

    .

--- addons/berp/socios/socio.py
+++ addons/berp/socios/socio.py
@@ -54,29 +54,30 @@ class berp_socio(models.Model):
         res = super(berp_socio, self).create(values)
         return res
 
-    nombre = fields.Char(string="Nombre")
-    tipo = fields.Many2many('berp.categoria',string="Tipo")
-    correo = fields.Char(string="Correo-e")
-    telefono = fields.Char(string="Teléfono")
-    especialidad = fields.Selection([('1', 'Velocidad'),('2', 'Fondo'),('3', 'Medio Fondo'),('4', 'Lanzamientos'),('5', 'Saltos'),('6', 'Marcha Atlética')],string='Especialidad')
-    nacionalidad = fields.Many2one('res.country',string="Nacionalidad",default="")
-    dni = fields.Char(string="DNI")
-    ficha = fields.Char(string="Ficha")
+    nombre = fields.Char(string="Nombre", required=True)
+    correo = fields.Char(string="Correo-e", required=True)
+    telefono = fields.Char(string="Teléfono", required=True)
+    nacionalidad = fields.Many2one('res.country',string="Nacionalidad",default=68, required=True)
+    dni = fields.Char(string="DNI", required=True)
     genero = fields.Selection([('1', 'Masculino'),('2', 'Femenino')],string='Sexo', required=True)
     fecha_nac = fields.Date(string="Fecha de Nacimiento", required=True)
-    fecha_alta = fields.Date(string="Fecha de Alta", required=True)
+    fecha_alta = fields.Date(string="Fecha de Alta", required=True, default=fields.Date.context_today)
     fecha_baja = fields.Date(string="Fecha de Baja")
     direccion = fields.Char(string="Dirección")
-    cuenta_banc = fields.Char(string="Cuenta Bancaria")
+    cuenta_banc = fields.Char(string="Cuenta Bancaria", required=True)
     nombre_cont = fields.Char(string="Contacto de Emergencia")
     telef_cont = fields.Char(string="Teléfono de Emergencia")
     imagen = fields.Binary('Imagen')
+    user = fields.Many2one('res.users',string="Usuario")
+
+
+    tipo = fields.Many2many('berp.categoria',string="Tipo")
+    especialidad = fields.Selection([('1', 'Velocidad'),('2', 'Fondo'),('3', 'Medio Fondo'),('4', 'Lanzamientos'),('5', 'Saltos'),('6', 'Marcha Atlética')],string='Especialidad')
+    ficha = fields.Char(string="Ficha")
+    categoria = fields.Char(string="Categoria")
     grupo_entreno = fields.Many2one('berp.grupo_entreno',string="Grupo Entreno")
     visible = fields.Boolean(compute="_comprobar_grupo_entreno",string="visible",default=False)
-    user = fields.Many2one('res.users',string="Usuario")
-    categoria = fields.Char(string="Categoria")
 
-#todo poner pais por defecto españa y luego mas a delante crear nueva clase para clubes que puede ser res.company hay que mirarselo
+#todo crear nueva clase para clubes que puede ser res.company hay que mirarselo
 
-# todo poner en el page de informacion del atleta en la vista poner un tree de sus mejores marcas y alguna cosa mas de información sobre el atleta... (mirar si es convincente)
 
