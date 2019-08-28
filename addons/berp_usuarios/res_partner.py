52393dbb7627aec7c382733d0edd5780be5cb8c7
Roberto <rmartin@leulit.com>
2019-08-28 15:03:56 +0200

    .

--- addons/berp_usuarios/res_partner.py
+++ addons/berp_usuarios/res_partner.py
@@ -11,17 +11,18 @@ class Partner(models.Model):
     _inherit = "res.partner"
 
 
+    user = fields.Many2one('res.users', string="Usuario")
     is_socio = fields.Boolean(string="Socio")
     is_atleta = fields.Boolean(string="Atleta")
-
-# socio
     fecha_nac = fields.Date(string="Fecha de Nacimiento", required=True)
     dni = fields.Char(string="DNI", required=True)
+
+    # socio
     fecha_alta = fields.Date(string="Fecha de Alta", required=True, default=fields.Date.context_today)
     fecha_baja = fields.Date(string="Fecha de Baja")
     socio_honorario = fields.Boolean(string="Honorario")
 
-# atleta
+    # atleta
     genero = fields.Selection([('1', 'Masculino'), ('2', 'Femenino')], string='Sexo', required=True)
     especialidad = fields.Selection([('1', 'Velocidad'),('2', 'Fondo'),('3', 'Medio Fondo'),('4', 'Lanzamientos'),('5', 'Saltos'),('6', 'Marcha Atlética')],string='Especialidad')
     ficha = fields.Char(string="Ficha")
