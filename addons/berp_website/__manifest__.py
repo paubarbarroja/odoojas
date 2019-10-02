911c0cc36b0ec35d9f394c6acf3ad3561b5974b6
Pau Barbarroja Claramunt <pau29jastermes@hotmail.com>
2019-10-02 22:22:55 +0200

    berp_website

--- addons/berp_website/__manifest__.py
+++ addons/berp_website/__manifest__.py
@@ -2,7 +2,7 @@
     'name': "Website",
     'summary': """Berp - Website""",
     'version': '1.0',
-    'depends': ['berp'],
+    'depends': ['berp','website'],
     'author': "Pau Barbarroja",
     'category': 'berp',
     'description': """
