21892585f474ef27745054baafb424202e5fb20e
paubarbarroja <pau29jastermes@hotmail.com>
2019-10-05 13:30:05 +0200

    .º

--- addons/berp_website/controllers/controllers.py
+++ addons/berp_website/controllers/controllers.py
@@ -4,4 +4,4 @@ from odoo import http
 class berp_website(http.Controller):
     @http.route('/inicio', auth='public', website=True)
     def index(self, **kw):
-        return "Hello world"
+        return http.request.render("inicio_berp",{})
