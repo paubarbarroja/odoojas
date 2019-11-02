08fca1c5c8797b9a5fdfe4d9f18dfa8d82f2fe9e
Pau Barbarroja Claramunt <pau29jastermes@hotmail.com>
2019-11-02 13:26:04 +0100

    res_company create

--- addons/berp_usuarios/company.py
+++ addons/berp_usuarios/company.py
@@ -8,3 +8,4 @@ class Company(models.Model):
 
 
     propagation_minimum_delta = fields.Integer('Minimum Delta for Propagation of a Date Change on moves linked together', default=1)
+    internal_transit_location_id = fields.Integer('internal_transit_location_id')
