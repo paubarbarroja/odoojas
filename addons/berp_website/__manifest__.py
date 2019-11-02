e96f2316304b60c81535f52a52111931d51ae45b
Pau Barbarroja Claramunt <pau29jastermes@hotmail.com>
2019-11-02 18:26:28 +0100

    product not sale in website

--- addons/berp_website/__manifest__.py
+++ addons/berp_website/__manifest__.py
@@ -2,7 +2,7 @@
     'name': "Website",
     'summary': """Berp - Website""",
     'version': '1.0',
-    'depends': ['berp','website'],
+    'depends': ['berp','website','website_sale'],
     'author': "Pau Barbarroja",
     'category': 'berp',
     'description': """
