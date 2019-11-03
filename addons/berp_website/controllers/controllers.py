0ca818d61cd82d25db51f98b2dc53d584dbf2132
paubarbarroja <pau29jastermes@hotmail.com>
2019-11-03 15:35:35 +0100

    website: new page --> formulari_soci

--- addons/berp_website/controllers/controllers.py
+++ addons/berp_website/controllers/controllers.py
@@ -2,6 +2,7 @@ from odoo import http
 
 
 class berp_website(http.Controller):
-    @http.route('/form_add_socio', auth='public', website=True)
+
+    @http.route('/formulari_soci', auth='public', website=True)
     def index(self, **kw):
         return http.request.render("berp_website.berp_20191103_1522_website",{})
