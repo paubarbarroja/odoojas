d1124aa6cb50f004d6981abfbc846de0d028d907
paubarbarroja <paubarbarroja@gmail.com>
2019-11-28 15:27:32 +0100

    .

--- addons/berp_website/controllers/controllers.py
+++ addons/berp_website/controllers/controllers.py
@@ -3,6 +3,6 @@ from odoo import http
 
 class berp_website(http.Controller):
 
-    @http.route('/formulari_soci', auth='user', website=True)
+    @http.route('/formulari_soci', auth='public', website=True)
     def index(self, **kw):
         return http.request.render("berp_website.berp_20191103_1522_website",{})
