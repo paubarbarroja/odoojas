6ef036fc5abd08dc745b98ec52a357d8bffcc29e
Pau Barbarroja Claramunt <pau29jastermes@hotmail.com>
2020-02-09 00:22:33 +0100

    vista usuario

--- addons/berp_usuarios/res_partner.py
+++ addons/berp_usuarios/res_partner.py
@@ -128,4 +128,5 @@ class Partner(models.Model):
     grupo_entreno = fields.Many2one('berp.grupo_entreno',string="Grupo Entreno")
     visible = fields.Boolean(compute="_comprobar_grupo_entreno",string="visible",default=False)
     tipo_ficha = fields.Selection([('esp', 'Ficha Española'), ('cat', 'Ficha Catalana'), ('fondo', 'Ficha Fondo y Ruta')], string='Tipo de Ficha')
-
+    descuento = fields.Boolean(string="Descuento")
+    descuento_tipo = fields.Char(string="Tipo descuento")
