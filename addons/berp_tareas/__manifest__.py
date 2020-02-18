6ff74da11d2747713d13388ef068779e1f22b1de
paubarbarroja <paubarbarroja@gmail.com>
2020-02-18 14:32:02 +0100

    cambiando ficha usuario

---
+++ addons/berp_tareas/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Tareas",
+    'summary': """Berp - Tareas""",
+    'version': '1.0',
+    'depends': ['berp'],
+    'author': "Pau Barbarroja",
+    'category': 'berp',
+    'description': """
+    """,
+    'data': [
+        'tareas.xml',
+        'menus.xml',
+    ],
+    'demo': [],
+    'application': True,
+}
+
