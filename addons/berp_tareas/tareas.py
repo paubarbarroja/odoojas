b59e900882660f89ea248b88300e0399a4d9ac67
paubarbarroja <paubarbarroja@gmail.com>
2020-02-18 14:48:07 +0100

    tareas

---
+++ addons/berp_tareas/tareas.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from datetime import date, datetime
+from odoo import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class berp_tarea(models.Model):
+    _name = "berp.tarea"
+
+    descripcion         = fields.Char('Descripción')
+    comentario          = fields.text('Comentario')
+    fecha_demanda       = fields.Date('Fecha de demanda')
+    solicitado_por      = fields.Many2one('res.users','Solicitado por')
+    tiempo_imputado     = fields.Float(compute='_get_tiempo_imputado',string='Tiempo imputado')
+    publicado           = fields.Boolean('Publicado')
