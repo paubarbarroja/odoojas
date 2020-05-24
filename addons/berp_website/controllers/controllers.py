0354c100c90f1f97ba758cc8e41669b604b5703b
Pau Barbarroja Claramunt <pau29jastermes@hotmail.com>
2020-05-24 14:22:26 +0200

    escola_atletisme

--- addons/berp_website/controllers/controllers.py
+++ addons/berp_website/controllers/controllers.py
@@ -1,8 +1,14 @@
 from odoo import http
 
 
-class berp_website(http.Controller):
+class berp_website_formulari_soci(http.Controller):
 
     @http.route('/formulari_soci', auth='public', website=True)
     def index(self, **kw):
         return http.request.render("berp_website.berp_20191103_1522_website",{})
+
+
+class berp_website_escola_atletisme(http.Controller):
+    @http.route('/escola_atletisme', auth='public', website=True)
+    def index(self, **kw):
+        return http.request.render("berp_website.berp_20200524_1414_website",{})
